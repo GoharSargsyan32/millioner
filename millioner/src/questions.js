1abf1d2fe43c136a16ddcf459868aeb2fd3d52ca
Gohar <GoharSargsyan32>
2024-11-05 01:04:12 +0400

    create queastions.js file for questions and answers

---
+++ millioner/src/questions.js
@@ -0,0 +1,54 @@
+const questions = [
+    {
+        question: "Which country is the birthplace of the Olympic Games?",
+        options: ["Italy", "France", "Greece", "China"],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        options: ["Atlantic", "Indian", "Arctic", "Pacific"],
+        correctAnswer: 3
+    },
+    {
+        question: "How many planets are in our solar system?",
+        options: ["8", "7", "9", "10"],
+        correctAnswer: 0
+    },
+    {
+        question: "Which animal is the symbol of the World Wildlife Fund (WWF)?",
+        options: ["Tiger", "Whale", "Panda", "Elephant"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which of these authors wrote 'Harry Potter'?",
+        options: ["J.R.R. Tolkien", "J.K. Rowling", "George Orwell", "Mark Twain"],
+        correctAnswer: 1
+    },
+    {
+        question: "What is the tallest waterfall in the world?",
+        options: ["Angel Falls", "Niagara Falls", "Victoria Falls", "Tugela Falls"],
+        correctAnswer: 0
+    },
+    {
+        question: "What is the capital of Australia?",
+        options: ["Sydney", "Melbourne", "Canberra", "Brisbane"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which artist created the painting 'Starry Night'?",
+        options: ["Claude Monet", "Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci"],
+        correctAnswer: 1
+    },
+    {
+        question: "What chemical element is the main component of diamonds?",
+        options: ["Carbonate", "Oxygen", "Hydrogen", "Carbon"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which organ in the human body filters blood?",
+        options: ["Lungs", "Liver", "Kidneys", "Heart"],
+        correctAnswer: 2
+    }
+];
+
+export default questions;
